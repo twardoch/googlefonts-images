11225a328b301ff4156a5b76dc11f5637d06f2bf
Adam Twardoch <adam+github@twardoch.com>
2025-06-25 23:14:26 +0200

    Auto-commit: Save local changes

--- build_images.py
+++ build_images.py
@@ -1,28 +1,27 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 
-from pathlib import Path
+import argparse
+import random
 from collections import OrderedDict
-import sys
-from sh import hb_view as hbv
+from pathlib import Path
+
 import fontTools.ttLib
 import fontTools.unicodedata as ucd
-import random
-import argparse
+from sh import hb_view as hbv
 
 base_folder = Path(Path(__file__).parent)
 
-class GoogleFontsImageGenerator(object):
 
+class GoogleFontsImageGenerator:
     def __init__(self, fonts_folder, images_folder=None, ppm=17):
         self.ppm = ppm
         self.fonts_folder = Path(fonts_folder)
         if images_folder:
             self.img_base_folder = images_folder
         else:
-            self.img_base_folder = Path(base_folder, 'img')
+            self.img_base_folder = Path(base_folder, "img")
         self.fonts = OrderedDict()
-        self.fonts_subfolders = ['apache', 'ofl', 'ufl']
+        self.fonts_subfolders = ["apache", "ofl", "ufl"]
         self.find_fonts()
 
     def get_script_unicodes(self, font):
@@ -30,20 +29,16 @@ class GoogleFontsImageGenerator(object):
         scripts_coverage = OrderedDict()
         unicodes = []
         for u in font.getBestCmap().keys():
-            if ucd.category(chr(u))[0] not in ('N', 'C') and u not in (0xFFFD, 0x0023):
+            if ucd.category(chr(u))[0] not in ("N", "C") and u not in (0xFFFD, 0x0023):
                 unicodes.append(u)
                 script = ucd.script(chr(u))
                 scripts[script] = scripts.get(script, 0) + 1
-                scripts_coverage[script] = scripts_coverage.get(
-                    script, []) + [u]
-        scripts = OrderedDict(sorted(
-            scripts.items(), key=lambda t: t[1], reverse=True
-        ))
-        for k in ('Zyyy', 'Zinh', 'Zzzz'):
+                scripts_coverage[script] = scripts_coverage.get(script, []) + [u]
+        scripts = OrderedDict(sorted(scripts.items(), key=lambda t: t[1], reverse=True))
+        for k in ("Zyyy", "Zinh", "Zzzz"):
             if k in scripts.keys():
                 del scripts[k]
-        scripts = list(scripts.keys()) if len(
-            scripts.keys()) else ['Zsym']
+        scripts = list(scripts.keys()) if len(scripts.keys()) else ["Zsym"]
         return scripts[0], scripts_coverage.get(scripts[0], unicodes)
 
     def get_script_sample_text_adv(self, font_path, words=5, chars=6):
@@ -52,38 +47,32 @@ class GoogleFontsImageGenerator(object):
         cats = OrderedDict()
         for u in unicodes:
             cat = ucd.category(chr(u))[0]
-            if cat in 'LNS' and ucd.script(chr(u)) == script:
+            if cat in "LNS" and ucd.script(chr(u)) == script:
                 cats[cat] = cats.get(cat, []) + [u]
         try:
-            unicodes = sorted(cats.items(), key=lambda t: len(
-                t[1]), reverse=True)[0][1]
+            unicodes = sorted(cats.items(), key=lambda t: len(t[1]), reverse=True)[0][1]
         except:
             unicodes = unicodes
-        lensample = words*chars
+        lensample = words * chars
         if lensample > len(unicodes):
             lensample = len(unicodes)
-        sample = "".join([
-            chr(u) for u in random.sample(unicodes, lensample)
-        ])
-        words = [
-            sample[0+i:chars+i]
-            for i in range(0, len(sample), chars)
-        ]
+        sample = "".join([chr(u) for u in random.sample(unicodes, lensample)])
+        words = [sample[0 + i : chars + i] for i in range(0, len(sample), chars)]
         newwords = []
         for wordi, word in enumerate(words):
-            if int(wordi/len(words)+0.7):
+            if int(wordi / len(words) + 0.7):
                 newwords.append(word.lower())
             else:
                 newwords.append(word.upper())
         return script, newwords
 
     def find_best_font(self, folder):
-        return sorted([str(p) for p in list(Path(folder).glob('**/*.?tf'))], key=len)[0]
+        return sorted([str(p) for p in list(Path(folder).glob("**/*.?tf"))], key=len)[0]
 
     def get_script_sample_text(self, font_path, base):
         script, words = self.get_script_sample_text_adv(font_path)
-        if script in ('Latn'):
-            words = [base[:6], 'RADHES', 'Hąmbrg', 'geföns'] + [words[-1]]
+        if script in ("Latn"):
+            words = [base[:6], "RADHES", "Hąmbrg", "geföns"] + [words[-1]]
         return script, " ".join(words)
 
     def find_fonts(self):
@@ -93,19 +82,19 @@ class GoogleFontsImageGenerator(object):
                 if folder.is_dir():
                     self.fonts[str(Path(folder).stem)] = self.find_best_font(folder)
 
-    def render_font(self, font_path, png_path, text, ppm=None, script='Latn'):
+    def render_font(self, font_path, png_path, text, ppm=None, script="Latn"):
         if not ppm:
             ppm = self.ppm
-        png = hbv(
+        hbv(
             font_file=font_path,
             font_size=ppm,
             font_ppem=ppm,
             text=text,
             script=script,
             margin=2,
-            output_format='png',
-            output_file=png_path
-            )
+            output_format="png",
+            output_file=png_path,
+        )
 
     def render_fonts(self):
         png_folder = Path(self.img_base_folder, str(self.ppm))
@@ -113,17 +102,16 @@ class GoogleFontsImageGenerator(object):
             png_folder.mkdir(parents=True)
         for base in list(self.fonts.keys()):
             font_path = self.fonts[base]
-            png_path = Path(png_folder, base + '.png')
+            png_path = Path(png_folder, base + ".png")
             script, sample_text = self.get_script_sample_text(font_path, base)
-            self.render_font(font_path, png_path,
-                             sample_text, self.ppm, script)
+            self.render_font(font_path, png_path, sample_text, self.ppm, script)
             print(base, script, sample_text, font_path)
 
-def cli():
 
+def cli():
     parser = argparse.ArgumentParser(
         prog="./build_images.py",
-        description="""Takes a folder of Google Fonts and builds images"""
+        description="""Takes a folder of Google Fonts and builds images""",
     )
     parser.add_argument(
         "-f",
@@ -131,7 +119,7 @@ def cli():
         required=True,
         metavar="folder",
         dest="fonts_folder",
-        help="""Folder with the local copy of https://github.com/google/fonts"""
+        help="""Folder with the local copy of https://github.com/google/fonts""",
     )
     parser.add_argument(
         "-i",
@@ -139,7 +127,7 @@ def cli():
         required=False,
         metavar="folder",
         dest="images_folder",
-        help="Folder in which the images will be written."
+        help="Folder in which the images will be written.",
     )
     parser.add_argument(
         "-p",
@@ -149,18 +137,17 @@ def cli():
         type=int,
         metavar="int",
         dest="ppm",
-        help="PPM size at which the images will be rendered."
+        help="PPM size at which the images will be rendered.",
     )
 
     args = parser.parse_args()
     print("Running with %s" % (repr(vars(args))))
 
     gfig = GoogleFontsImageGenerator(
-        fonts_folder=args.fonts_folder,
-        images_folder=args.images_folder,
-        ppm=args.ppm
+        fonts_folder=args.fonts_folder, images_folder=args.images_folder, ppm=args.ppm
     )
     gfig.render_fonts()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     cli()
